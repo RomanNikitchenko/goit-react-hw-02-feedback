182203712febdff51ce43427cc0d319c11dbd1cc
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-05-09 23:24:05 +0300

    ht2-1

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -15,18 +15,11 @@ class Statistics extends React.Component {
   countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
     const sum = good + neutral + bad;
     return Math.round(sum > 0 ? (good * 100) / sum : 0);
-  }
+  };
 
-  good = () => {
-    this.setState(state => ({ good: state.good + 1 }));
-  };
-    
-  neutral = () => {
-    this.setState(state => ({ neutral: state.neutral + 1 }));
-  };
-    
-  bad = () => {
-    this.setState(state => ({ bad: state.bad + 1 }));
+  handleChange = evt => {
+    const { name } = evt.target;
+    this.setState(state => ({ [name]: state[name] + 1 }));
   };
 
   render() {
@@ -34,17 +27,19 @@ class Statistics extends React.Component {
 
     return (
       <div>
-        <button onClick={this.good}>Good</button>
-        <button onClick={this.neutral}>Neutral</button>
-        <button onClick={this.bad}>Bad</button>
+        <button name="good" onClick={this.handleChange}>Good</button>
+        <button name="neutral" onClick={this.handleChange}>Neutral</button>
+        <button name="bad" onClick={this.handleChange}>Bad</button>
+
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+
         <p>Total: {this.countTotalFeedback(this.state)}</p>
         <p>PositiveFeedback: {this.countPositiveFeedbackPercentage(this.state)}%</p>
       </div>
-    );
-  }
+    )
+  };
 };
 
 export default Statistics;
