ce919c12493f3b7e7a7f0cde8fe2145939646416
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-05-10 17:32:07 +0300

    ht2.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,47 @@
-import Statistics from './feedback/feedback'
+import React from "react";
+import Statistics from "./Statistics/Statistics";
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        // display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Statistics />
-      {/* React homework template */}
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  countTotalFeedback = ({ good, neutral, bad }) => {
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
+    const sum = good + neutral + bad;
+    return Math.round(sum > 0 ? (good * 100) / sum : 0);
+  };
+
+  handleChange = evt => {
+    const { name } = evt.target;
+    this.setState(state => ({ [name]: state[name] + 1 }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <button name="good" onClick={this.handleChange}>Good</button>
+        <button name="neutral" onClick={this.handleChange}>Neutral</button>
+        <button name="bad" onClick={this.handleChange}>Bad</button>
+
+        <Statistics
+          good={ good }
+          neutral={ neutral }
+          bad={ bad }
+          total={ this.countTotalFeedback(this.state) }
+          positivePercentage={ this.countPositiveFeedbackPercentage(this.state) }
+        />
+      </div>
+    )
+  };
 };
+
+export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,14 @@
+const Statistics = ( {good, neutral, bad, total, positivePercentage} ) => {
+    return (
+        <div>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+
+            <p>Total: {total}</p>
+            <p>PositiveFeedback: {positivePercentage}%</p>
+        </div>
+    )
+};
+
+export default Statistics;
